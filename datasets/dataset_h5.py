3a50c18e0cc3b15e94368e367dc7f37d2ded2855
julianostermaier <julian.ostermaier@gmx.net>
2022-10-28 10:55:08 +0100

    changes to HDF5Dataset without caching mechanism

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -8,6 +8,7 @@ import re
 import pdb
 import pickle
 import helpers
+import openslide
 
 from torch.utils.data import Dataset, DataLoader, sampler
 from torchvision import transforms, utils, models
@@ -175,124 +176,79 @@ class HDF5Dataset(Dataset):
     Input params:
         file_path: Path to the folder containing the dataset (one or multiple HDF5 files).
         recursive: If True, searches for h5 files in subdirectories.
-        load_data: If True, loads all the data immediately into RAM. Use this if
-            the dataset is fits into memory. Otherwise, leave this at false and 
-            the data will load lazily.
-        data_cache_size: Number of HDF5 files that can be cached in the cache (default=3).
         transform: PyTorch transform to apply to every data instance (default=None).
     """
-    def __init__(self, file_path, recursive, load_data, data_cache_size=3, transform=None):
+    def __init__(self, file_path, wsi_path, recursive, load_data, data_cache_size=3, transform=None):
         super().__init__()
         self.data_info = []
-        self.data_cache = {}
-        self.data_cache_size = data_cache_size
         self.transform = transform
 
-        # Search for all h5 files
+        # Search for all h5 and svs files
         p = Path(file_path)
+        w = Path(wsi_path)
         assert(p.is_dir())
+        assert(w.is_dir())
         if recursive:
-            files = sorted(p.glob('**/*.h5'))
+            files = sorted(p.glob('**/*.h5'))[0:10]
+            wsis = sorted(w.glob('**/*.svs'))
         else:
             files = sorted(p.glob('*.h5'))
+            wsis = sorted(w.glob('*.svs'))
         if len(files) < 1:
             raise RuntimeError('No hdf5 datasets found')
-
-        for h5dataset_fp in files:
-            self._add_data_infos(str(h5dataset_fp.resolve()), load_data)
+            
+        self.wsi_files = [str(f.resolve()) for f in wsis]
+                          
+        for i, h5dataset_fp in enumerate(files):
+            self._add_data_infos(str(h5dataset_fp.resolve()), i)
             
     def __getitem__(self, index):
         # get coords of patch
-        x = self.get_data("coords", index)
-        if self.transform:
-            x = self.transform(x)
-        else:
-            x = torch.from_numpy(x)
+        x = self.get_data(index)
 
-        return x
+        return self.transform(x)
 
     def __len__(self):
-        return len(self.get_data_infos('data'))
+        return len(self.data_info)
     
-    def _add_data_infos(self, file_path, load_data):
-        with h5py.File(file_path) as h5_file:
+    def _add_data_infos(self, file_path, i):
+        with h5py.File(file_path, "r") as h5_file:
             # get metadata for WSI
-			dset = h5_file['coords']
-			patch_level = f['coords'].attrs['patch_level']
-			patch_size = f['coords'].attrs['patch_size']
-			wsi = openslide.open_slide(slide_file_path) # ??
+            patch_level = h5_file['coords'].attrs['patch_level']
+            patch_size = h5_file['coords'].attrs['patch_size']
 
-			# iterate over coordinates of all patches in WSI
-			for patch_idx in range(dset.len()):
-				coord = dset[patch_idx]
-				idx = -1
-				if load_data:
-					# add data to the data cache
-					img = wsi.read_region(coord, patch_level, (patch_size, patch_size)).convert('RGB')
-					idx = self._add_to_cache(img, file_path)
-				
-				# add information to data_info
-				self.data_info.append({'file_path': file_path, 'type', 'cache_idx': idx, 'coords': coord, 'patch_level': patch_level, 'patch_size': patch_size})
-
-    def _load_data(self, file_path):
-        """Load data to the cache given the file
-        path and update the cache index in the
-        data_info structure.
+            # iterate over coordinates of all patches in WSI
+            print(h5_file['coords'].len())
+            for patch_idx in range(h5_file['coords'].len()):
+                coord = h5_file['coords'][patch_idx]
+                
+                # add information to data_info
+                self.data_info.append({'file_path': file_path, 'wsi_path': self._find_wsi(file_path), 'coords': coord, 'patch_level': patch_level, 'patch_size': patch_size})
+                
+    def _find_wsi(self, file_path):
+        """match .h5 file to the corresponding wsi file_path
         """
-        with h5py.File(file_path) as h5_file:
-            # get metadata for WSI
-			dset = h5_file['coords']
-			wsi = openslide.open_slide(slide_file_path) # ??
-
-			# iterate over coordinates of all patches in WSI
-			for patch_idx in range(dset.len()):
-				coord = dset[patch_idx]
-				# add data to the data cache
-				img = wsi.read_region(coord, patch_level, (patch_size, patch_size)).convert('RGB')
-				idx = self._add_to_cache(img, file_path)
-
-				# find the beginning index of the hdf5 file we are looking for
-				file_idx = next(i for i,v in enumerate(self.data_info) if v['file_path'] == file_path)
-
-				# the data info should have the same index since we loaded it in the same way
-				self.data_info[file_idx + idx]['cache_idx'] = idx
-
-        # remove an element from data cache if size was exceeded
-        if len(self.data_cache) > self.data_cache_size:
-            # remove one item from the cache at random
-            removal_keys = list(self.data_cache)
-            removal_keys.remove(file_path)
-            self.data_cache.pop(removal_keys[0])
-            # remove invalid cache_idx
-            self.data_info = [{'file_path': di['file_path'], 'type': di['type'], 'coords': di['coords'], 'cache_idx': -1, 'patch_level': di['patch_level'], 'patch_size': di['patch_size']} if di['file_path'] == removal_keys[0] else di for di in self.data_info]
-
-    def _add_to_cache(self, data, file_path):
-        """Adds data to the cache and returns its index. There is one cache
-        list for every file_path, containing all datasets in that file.
-        """
-        if file_path not in self.data_cache:
-            self.data_cache[file_path] = [data]
-        else:
-            self.data_cache[file_path].append(data)
-        return len(self.data_cache[file_path]) - 1
-
-    def get_data_infos(self, type):
-        """Get data infos belonging to a certain type of data.
-        """
-        data_info_type = [di for di in self.data_info if di['type'] == type]
-        return data_info_type
-
-    def get_data(self, type, i):
-        """Call this function anytime you want to access a chunk of data from the
-            dataset. This will make sure that the data is loaded in case it is
-            not part of the data cache.
-        """
-        fp = self.get_data_infos(type)[i]['file_path']
-        if fp not in self.data_cache:
-            self._load_data(fp)
         
-        # get new cache_idx assigned by _load_data_info
-        cache_idx = self.get_data_infos(type)[i]['cache_idx']
-        return self.data_cache[fp][cache_idx]
+        # returns only filename of file in file_path without .h5 ending
+        file_name = file_path.split('/')[-1].split('.')[0]
+        if len(file_name) < 1:
+            raise Exception("could not extract file_name")
+        
+        # find wsi path for file_name
+        wsi_match = [f for f in self.wsi_files if file_name in f]
+        if len(wsi_match) != 1:
+            raise Exception("could not match wsi to file_name")
+            
+        return wsi_match[0]
+
+    def get_data(self, i):
+        """Call this function anytime you want to access a chunk of data
+        """
+        patch = self.data_info[i]
+        
+        wsi = openslide.open_slide(patch['wsi_path'])
+        img = wsi.read_region(patch['coords'], patch['patch_level'], (patch['patch_size'], patch['patch_size'])).convert('RGB')
+
+        return img
 
 
